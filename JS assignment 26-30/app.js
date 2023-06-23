1faf417f612a2553799815ff7ed6403fc6313b0e
Maha_Zaidi <124033522+Maha234Zaidi@users.noreply.github.com>
2023-06-24 01:43:36 +0500

    Add files via upload

---
+++ JS assignment 26-30/app.js
@@ -0,0 +1,73 @@
+// ==========================JS ASSIGNMENT 26-30=============================//
+
+//ques 1
+//ans
+
+//  var userInput = +prompt("enter number");
+// var round = Math.round(userInput);
+// var floor = Math.floor(userInput);
+// var ceil = Math.ceil(userInput);
+// document.write("number: " + userInput + "<br>" + "round off value: " + round + "<br>" + "floor value: " + floor + "<br>" + "ceil value: " + ceil); 
+
+// ques 2
+//ans
+
+// var number = +prompt("enter number");
+// var round = Math.round(number);
+// var floor = Math.floor(number);
+// var ceil = Math.ceil(number);
+// document.write("number: " + number + "<br>" + "round off value: " + round + "<br>" + "floor value: " + floor + "<br>" + "ceil value: " + ceil);
+
+// ques 3
+//ans
+
+// var num = +prompt("enter number");
+// var absolute = Math.abs(num);
+// document.write("The absolute value of " + num + " is " + absolute);
+
+// ques 4
+//ans
+
+// var randomNo =Math.random();
+// var maths = (randomNo * 6) + 1;
+// var diceNumber = Math.floor(maths);
+// document.write("random dice value " + diceNumber);
+
+// ques 5
+// ans
+// var valueOfCoin = Math.random() * 3;
+// var floor = Math.floor(valueOfCoin);
+// if (floor < 2){
+//     document.write(floor + "<br>" + " random coin value: " + "Tails" )
+// }
+// else{
+//     document.write(floor + "<br>" + " random coin value: " + "Heads")
+// }
+
+// ques 6
+// ans
+// var randomNum = Math.random() * 100 + 1 ;
+// var floor = Math.floor(randomNum);
+// document.write("random number between 1 and 100: " + floor );
+
+// ques 7
+// ans 
+// var userWeight = prompt("enter your weight in kilograms");
+// var parse = parseFloat(userWeight);
+// document.write("The weight of user is " + parse.toFixed(1) + " kilograms");
+
+// ques 8
+// ans
+// var randomNo =Math.ceil( (Math.random() * 10)) ;
+// document.write(randomNo);
+// var askUser = +prompt("Input a number between 1 to 10");
+// if(askUser === randomNo){
+//     alert("Congratulations!");}
+
+// else{
+//     alert("Try again!");
+//
+
+
+
+// ======================completed=======================//
